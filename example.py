4e17137edf2e3d28b8b25ced09bc292da41ee2cb
Frank Yao <baniu.yao@gmail.com>
2012-09-05 00:33:44 +0800

    'use iter instead of while loop'

--- example.py
+++ example.py
@@ -1,27 +1,24 @@
 #!/usr/bin/env python
 
-from backReader import BackwardsReader as BackReader
+from backReader import BackwardsReaderIter
 from parselog import LogParser
 import time, re, sys
 
 if __name__ == '__main__':
-	file_name = sys.argv[1]
+	#file_name = sys.argv[1]
+	file_name = 'example_access.log'
 	current_time = time.time()
 	key = (
-		('[ ,](20[06])[^\d]','2XX','count'), #2XX
-		('[ ,](4[90][349])[^\d]','4XX','count'), #4XX
-		('[ ,](50[023])[^\d]','5XX','count'), #5XX
-		(' (\d*)( [^ ]*){3}$','resp','sum'), #resp
+		('" (2[\d]{2}) ', '2XX', 'count'),
+		('" (4[\d]{2}) ', '4XX', 'count'),
+		('" (5[\d]{2}) ', '5XX', 'count'),
 		(' (\d*)( [^ ]*){2}$','traffic','sum'), #traffic
 	)
-	lp = LogParser(keyword_list=key,re_time='\d{2}\/\w{3}\/\d{4}:\d{2}:\d{2}:\d{2}', str_time='%d/%b/%Y:%H:%M:%S')
-	o = open(file_name)
-	br = BackReader(o)
-	ip_set = set()
-	while True:
-		line = br.readline().strip()
+	lp = LogParser(keyword_list=key,re_time='\d{2}\/\w{3}\/\d{4}:\d{2}:\d{2}:\d{2}', str_time='%d/%b/%Y:%H:%M:%S', period=(0,time.time()))
+	bri = BackwardsReaderIter(file_name)
+	for line in bri.backread():
 		if lp.inPeriod(line):
 			lp.parseLine(line)
 		else:
 			break
-	o.close()
+	print lp.getResult()
